c71a2f121b40650a548139c2fba7ae730e2fd18e
Imalittlecat <den.kidyamkin@mail.ru>
2024-12-21 09:25:10 +0500

    add app.js

---
+++ js/app.js
@@ -0,0 +1,27 @@
+let addMessage = document.querySelector('.message')
+let addButton = document.querySelector('.add')
+let todo = document.querySelector('.todo')
+let del = document.querySelector('.delete')
+let todoList = []
+
+if (localStorage.getItem('todo')) {
+    todoList = JSON.parse(localStorage.getItem('todo'))
+    displayMessages()
+}
+
+addButton.addEventListener('click', function () {
+    if (!addMessage.value) return 
+    let newTodo = {
+        todo: addMessage.value,
+        checked: false,
+        import: false
+    } 
+    todoList.push(newTodo)
+    displayMessages()
+    localStorage.setItem('todo', JSON.stringify(todoList))
+    addMessage.value = ''
+})
+
+function displayMessages() {
+    
+}
