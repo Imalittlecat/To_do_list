c9faeb32b89367404bd779659275a40443373ca9
Imalittlecat <den.kidyamkin@mail.ru>
2024-12-28 11:03:07 +0500

    add important

--- js/app.js
+++ js/app.js
@@ -21,32 +21,17 @@ addButton.addEventListener('click', function () {
     addMessage.value = ''
 })
 
-function displayMessages() {
-    let displayMessage = ''
-
-    if (todoList.length === 0)  todo.innerHTML = 'Задач нет'
-    
-    todoList.forEach((item, i) => {
-        displayMessage += `
-            <li>
-                <input type="checkbox" id="item_${i}" ${
-            item.checked ? 'checked' : ''
-        }>
-                <label for="item_${i}" class="${i}" ${
-            item.important ? 'important' : ''   
-        }">
-                ${item.todo}</label>
-
-                <img class = 'delete' src="./icons/delete.png" alt="delete">
-            </li>
-        `
-        todo.innerHTML = displayMessage
-        delTask()
+function setImportant() {
+    const importantButtons = document.querySelectorAll('.important-btn')
+    importantButtons.forEach((btn) => {
+        btn.addEventListener('click', (event) => {
+            const index = event.target.dataset.index
+            todoList[index].important = !todoList[index].important
+            localStorage.setItem('todo', JSON.stringify(todoList))
+            displayMessages()   
+        })
     })
-} 
-
-
-
+}
 
 function delTask() {
     const deleteButtons = document.querySelectorAll('.delete')
@@ -58,6 +43,47 @@ function delTask() {
             displayMessages()
         })
     })
+}
+
+// Функция для отображения всех задач
+function displayMessages() {
+    // Если массив задач пуст, показываем сообщение 'Задач нет'
+    if (!todoList.length) {
+        todo.innerHTML = 'Задач нет'
+        return
+    }
+
+    // Строка, в которую соберем HTML со всеми задачами
+    let displayMessage = ''
+    // Перебираем все объекты задач из массива todoList
+    todoList.forEach((item, i) => {
+        // Формируем HTML-разметку для каждой задачи
+        displayMessage += `
+        <li>
+            <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
+
+            <label for='item_${i}' class='${item.important ? 'important' : ''}'>
+                ${item.todo}
+            </label>
+
+            <div class="tools">
+                <img class='edit' data-index='${i}' src='./icons/edit.png' alt='edit'>
+                <img class='important-btn' data-index='${i}' src='./icons/important.png' alt='important'>
+                <img class='delete' data-index='${i}' src='./icons/delete.png' alt='delete'>
+            </div>
+        </li>
+        `  
+    })
+
+    todo.innerHTML = displayMessage
+    delTask()
+    setImportant()
+} 
+
+
+
+function editTask() {
+
 }
 
 // const todoListCons = JSON.parse(localStorage.getItem('todo'))
